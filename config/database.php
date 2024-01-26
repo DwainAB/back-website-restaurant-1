fec62be5dee3d8cb2f2b96aff9266a5a7e91bdbc
dwain93 <74243765+dwain93@users.noreply.github.com>
2024-01-26 23:03:58 +0100

    modif

--- config/database.php
+++ config/database.php
@@ -1,13 +1,18 @@
 <?php
 
-$host = "localhost";
-$dbname = "db-wok-rosny";
-$username = "root";
-$password = "root";
+// Spécifier le chemin absolu vers votre fichier de base de données SQLite
+// Assurez-vous que le chemin d'accès et les permissions sont corrects
+$path = __DIR__ . '/data.sqlite';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;", $username, $password);
+    // Créez un nouvel objet PDO pour la connexion à la base de données SQLite
+    $pdo = new PDO('sqlite:' . $path);
+    // Configurez les attributs PDO comme nécessaire
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // La ligne suivante est pour le débogage, vous pouvez la commenter ou la supprimer plus tard
+    echo 'Connexion réussie à la base de données SQLite.';
 } catch (PDOException $e) {
     die("Erreur de connexion à la base de donnée : " . $e->getMessage());
 }
+
+// Votre code pour interagir avec la base de données peut aller ici
