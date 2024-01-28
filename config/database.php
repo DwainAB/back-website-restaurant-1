dbe2f96f07de3e13508885a8b0c4a9d7beed3545
dwain93 <74243765+dwain93@users.noreply.github.com>
2024-01-28 22:49:50 +0100

    ajout de la nouvelle base de donnée

--- config/database.php
+++ config/database.php
@@ -1,15 +1,25 @@
 <?php
 
-// Spécifier le chemin absolu vers votre fichier de base de données SQLite
-// Assurez-vous que le chemin d'accès et les permissions sont corrects
-$path = __DIR__ . '/my_database.sqlite';
+// Les informations de connexion à la base de données MySQL
+$host = 'sql11.freemysqlhosting.net';
+$db   = 'sql11680370'; // Nom de la base de données
+$user = 'sql11680370'; // Nom d'utilisateur
+$pass = 'Q9E7b1CLh7'; // Vous devriez chercher le mot de passe dans votre e-mail
+
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
 try {
-    // Créez un nouvel objet PDO pour la connexion à la base de données SQLite
-    $pdo = new PDO('sqlite:' . $path);
-    // Configurez les attributs PDO comme nécessaire
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Créez un nouvel objet PDO pour la connexion à la base de données MySQL
+    $pdo = new PDO($dsn, $user, $pass, $options);
     // La ligne suivante est pour le débogage, vous pouvez la commenter ou la supprimer plus tard
+    echo "Connexion à la base de données MySQL établie avec succès.";
 } catch (PDOException $e) {
     die("Erreur de connexion à la base de donnée : " . $e->getMessage());
 }
